f578c4bcfb5ccf6dcae1355f61ce8cb17fe8b005
Ashok Koyi <ashok.koyi@gmail.com>
2021-09-15 03:47:04 +0530

    now the new script checks to see if the book already exists before creating

--- init-new-book.sh
+++ init-new-book.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
-then
+if [ $# -lt 1 ]; then
     echo -e "Enter book name\n"
     exit 1
 fi
 
 book_name=`echo "$*" | perl -pe 's/^\s*//g and s/\s*$//g'`
-echo $book_name
+
 # trim spaces at edges, replace upper case with lower, spaces with `-` & remove quotes
 book_folder_name_sanitised=`echo $book_name | perl -pe 's/^\s*//g and s/\s*$//g and s/([A-Z])/\L$1/g and s/\s+/-/g and s/[\x27"]//g'`
-echo $book_folder_name_sanitised
+
 book_folder_path="repo/$book_folder_name_sanitised"
 
+if [[ -d $book_folder_path ]]; then
+    echo -e "\n\t¯\_ (ツ)_/¯\n"
+    echo -e "Book directory already exists under this path\n"
+    echo $book_folder_path
+    exit 1
+fi
+
 mkdir $book_folder_path
+
+# useful for placing original pdfs so that they wont be checked into repo by accident
+mkdir $book_folder_path/gen
 
 # add appropriate header
 echo "---
